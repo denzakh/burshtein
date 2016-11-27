87edef4f3ab221ceabae9d40160b03449b5e3409
denzakh <den.zakh@gmail.com>
2016-11-27 20:22:10 +0300

    синхронизация scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,47 @@
+"use strict";
+
+// переменные (модули)
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+
+var rename = require("gulp-rename");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync");
+var mqpacker = require("css-mqpacker");
+var minify = require('gulp-csso');
+var imagemin = require('gulp-imagemin');
+var del = require("del");
+var run = require('run-sequence');
+var fs = require('fs');
+
+
+// сборка стилей
+gulp.task('sass', function() {
+    return gulp.src("app/scss/*.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss([
+          autoprefixer({browsers: [
+            "last 1 version",
+            "last 2 Chrome versions",
+            "last 2 Firefox versions",
+            "last 2 Opera versions",
+            "last 2 Edge versions"
+          ]}),
+          mqpacker({
+            sort: false
+          })
+        ]))
+        .pipe(gulp.dest("app/css"))
+        .pipe(minify())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
+});
+
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -13,12 +54,5 @@ gulp.task('serve', ['sass'], function() {
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream());
-});
 
 gulp.task('default', ['serve']);
